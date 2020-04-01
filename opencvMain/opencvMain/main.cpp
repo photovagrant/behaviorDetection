c1bc6bedd30bb217fdb35fb4bd407f5185a6ad28
photovagrant <photovagrant.pda@gmail.com>
2020-04-01 23:20:46 +0800

    append macos license to using camera

---
+++ opencvMain/opencvMain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvMain
+//
+//  Created by assam on 2020/3/30.
+//  Copyright © 2020 assam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
